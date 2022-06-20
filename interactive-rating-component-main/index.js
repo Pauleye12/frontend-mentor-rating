f3d144d742402c6c40a773efa06cb76703b90fb3
CRIMSON <olueatoworosanwo@gmail.com>
2022-06-20 10:57:09 +0100

    Added more styling, fixed some issues in js

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -1,35 +1,49 @@
-const rateEl = document.querySelectorAll(".rate")
-const submitEl = document.querySelector(".submit-btn")
-const outputEl = document.querySelector(".choice")
-const firstEl = document.getElementById("first")
-const secondEl = document.getElementById("second")
-let num = 0
-let rate =0
-console.log(rateEl)
+const rateEl = document.querySelectorAll(".rate");
+const submitEl = document.querySelector(".submit-btn");
+const outputEl = document.querySelector(".choice");
+const firstEl = document.getElementById("first");
+const secondEl = document.getElementById("second");
+const wrapper = document.querySelector(".wrapper");
+let num = 0;
+let rate = 0;
+// console.log(rateEl);
 
+rateEl.forEach((rates) => {
+    rates.addEventListener("click", function (e) {
+        num = e.target.innerText;
+        // addActive(num);
 
-Array.from(rateEl).forEach(rates => {
-    rates.addEventListener("click", (e) =>{
-        num = (e.target.innerText)
-        addActive(num)
-        console.log(num)
-        outputEl.textContent = `you selected ${num} out of 5`
-    })
-})
+        // You can do this intead of your addActive function
+        // what i did here is to kuku remove active from all
+        // of them
+        rateEl.forEach((rate) => {
+            rate.classList.remove("active");
+        });
+        // then add the active class to the one that is
+        //currently clicked
+        this.classList.add("active");
+        console.log(num);
+        outputEl.textContent = `You selected ${num} out of 5`;
+    });
+});
 
-submitEl.addEventListener("click", (e)=>{
-    firstEl.classList.add("display");
-    secondEl.classList.replace("display", "thank-you")
+submitEl.addEventListener("click", (e) => {
+    // if user didnt select anything,
+    // do nothing
+    if (num == 0) return;
+    // else, add a class that would rotate the card
+    wrapper.classList.add("feedback");
+});
 
-})
-
-function addActive(ratess){
-    Array.from(rateEl).forEach(rates => {
-        if(rates.innerText === (ratess)){
-            console.log("me")
-            rates.classList.add("active")
-        } else {
-            rates.classList.remove("active")
-        }
-    })
-}
+// function addActive(ratess) {
+//     // no need to make a normal array from the rateEl elemenets
+//     // they are already in an array by default
+//     rateEl.forEach((rates) => {
+//         if (rates.innerText === ratess) {
+//             console.log("me");
+//             rates.classList.add("active");
+//         } else {
+//             rates.classList.remove("active");
+//         }
+//     });
+// }
